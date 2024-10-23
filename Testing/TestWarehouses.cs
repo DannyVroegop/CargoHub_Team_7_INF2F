752588faac2db476bd85b8cc33f3c97a4eba5091
Timtimthethird <125371174+Timtimthethird@users.noreply.github.com>
2024-10-23 15:00:28 +0200

    test GET locations
    ook Get locations/"code"

--- Testing/TestWarehouses.cs
+++ Testing/TestWarehouses.cs
@@ -8,11 +8,11 @@ using System.Collections.Generic;
 
 namespace Test;
 [TestClass]
-public class UnitTestWarehouses
+public class UnitTestWarehouses_Locations
 {
     private readonly HttpClient _client;
     private const string ApiKey = "a1b2c3d4e5";
-    public UnitTestWarehouses()
+    public UnitTestWarehouses_Locations()
     {
         _client = new HttpClient();
         _client.BaseAddress = new Uri("http://127.0.0.1:3000");
@@ -75,10 +75,11 @@ public class UnitTestWarehouses
         Assert.AreEqual(responseBodyonenowhite, expectedJsonnowhite);
 
         var requestlocations = new HttpRequestMessage(HttpMethod.Get, "/api/v1/warehouses/1/locations");
-        // Without key should be false
         requestlocations.Headers.Add("API_KEY", ApiKey);
         var locs = await _client.SendAsync(requestlocations);
+
         Assert.IsTrue(locs.IsSuccessStatusCode, "API call was not successful");
+
         string responseBodylocs = await locs.Content.ReadAsStringAsync();
         string singlelocation = "";
         using (JsonDocument doc = JsonDocument.Parse(responseBodylocs))
@@ -111,4 +112,49 @@ public class UnitTestWarehouses
         Assert.AreEqual(expectedJsonlocnowhite, responseBodylocnowhite);
 
      }
+
+     [TestMethod]
+     public async Task TestGetLocations()
+     {
+        var requestall_wo_key = new HttpRequestMessage(HttpMethod.Get, "/api/v1/locations");
+        // Without key should be false
+        var wo_key = await _client.SendAsync(requestall_wo_key);
+        Assert.IsFalse(wo_key.IsSuccessStatusCode, "API call was successful (It should not be)");
+
+
+        var requestall = new HttpRequestMessage(HttpMethod.Get, "/api/v1/locations");
+        requestall.Headers.Add("API_KEY", ApiKey);
+        var all = await _client.SendAsync(requestall);
+        Assert.IsTrue(all.IsSuccessStatusCode, "API call was not successful");
+        // Check content
+        string responseBodyall = await all.Content.ReadAsStringAsync();
+        Assert.IsNotNull(responseBodyall, "Response body is null");
+
+        // Request one
+        var requestone = new HttpRequestMessage(HttpMethod.Get, "/api/v1/locations/A.2.4");
+        requestone.Headers.Add("API_KEY", ApiKey);
+        var one = await _client.SendAsync(requestone);
+        // response
+        Assert.IsTrue(one.IsSuccessStatusCode, "API call was not successful");
+        // content
+        string responseloc = await one.Content.ReadAsStringAsync();
+        Assert.IsNotNull(responseloc, "Response body is null");
+
+        string expectedlocation = "{\n" +
+                    "    \"warehouse_id\": 1,\n" +
+                    "    \"code\": \"A.2.4\",\n" +
+                    "    \"name\": \"Row: A, Rack: 2, Shelf: 4\",\n" +
+                    "    \"created_at\": \"2024-04-24 10:23:44\",\n" +
+                    "    \"updated_at\": \"2024-04-24 10:23:44\"\n" +
+                    "}";
+        string responseBodylocnowhite = responseloc.Replace(" ", "")
+                     .Replace("\t", "")
+                     .Replace("\n", "")
+                     .Replace("\r", "");
+        string expectedJsonlocnowhite = expectedlocation.Replace(" ", "")
+                     .Replace("\t", "")
+                     .Replace("\n", "")
+                     .Replace("\r", "");
+        Assert.AreEqual(expectedJsonlocnowhite, responseBodylocnowhite);
+    }
 }
