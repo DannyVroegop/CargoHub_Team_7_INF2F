3f70fae313dd007772321e9961c8ef0801b35871
Timtimthethird <125371174+Timtimthethird@users.noreply.github.com>
2024-10-23 13:35:37 +0200

    testGET single warehouse

--- Testing/TestWarehouses.cs
+++ Testing/TestWarehouses.cs
@@ -1,6 +1,7 @@
 global using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.IO;
 using System;
+using System.Text.Json;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Collections.Generic;
@@ -21,14 +22,59 @@ public class UnitTestWarehouses
      public async Task TestGetWarehouses()
      {
 
+        var requestall_wo_key = new HttpRequestMessage(HttpMethod.Get, "/api/v1/warehouses");
+        // Without key should be false
+        var wo_key = await _client.SendAsync(requestall_wo_key);
+        Assert.IsFalse(wo_key.IsSuccessStatusCode, "API call was successful (It should not be)");
+
+        // Check response
         var requestall = new HttpRequestMessage(HttpMethod.Get, "/api/v1/warehouses");
         requestall.Headers.Add("API_KEY", ApiKey);
         var all = await _client.SendAsync(requestall);
-        // Check response
         Assert.IsTrue(all.IsSuccessStatusCode, "API call was not successful");
-
         // Check content
-        string responseBody = await all.Content.ReadAsStringAsync();
-        Assert.IsNotNull(responseBody, "Response body is null");
+        string responseBodyall = await all.Content.ReadAsStringAsync();
+        Assert.IsNotNull(responseBodyall, "Response body is null");
+
+        // Check one
+        var requestone = new HttpRequestMessage(HttpMethod.Get, "/api/v1/warehouses/1");
+        requestone.Headers.Add("API_KEY", ApiKey);
+        var one = await _client.SendAsync(requestone);
+        // response
+        Assert.IsTrue(one.IsSuccessStatusCode, "API call was not successful");
+        // content
+        string responseBodyone = await one.Content.ReadAsStringAsync();
+        Assert.IsNotNull(responseBodyone, "Response body is null");
+        string expectedJson = @"{
+            ""id"": 1,
+            ""code"": ""RTRSPOL"",
+            ""name"": ""Rotterdam Spaanse Polder Giessenweg"",
+            ""address"": ""Giessenweg 20"",
+            ""zip"": ""3044 BA"",
+            ""city"": ""Rotterdam"",
+            ""province"": ""Zuid-Holland"",
+            ""country"": ""NL"",
+            ""contact"": {
+                ""name"": ""John Doe"",
+                ""phone"": ""06-12345678"",
+                ""email"": ""john@rtrspol.warehousing.eu""
+            },
+            ""created_at"": ""2024-02-03 13:12:00"",
+            ""updated_at"": ""2024-02-03 13:12:00""
+        }";
+        // var responseJsonDocument = JsonDocument.Parse(responseBodyone);
+        // var expectedJsonDocument = JsonDocument.Parse(expectedJson);
+        string responseBodyonenowhite = responseBodyone.Replace(" ", "")
+                     .Replace("\t", "")
+                     .Replace("\n", "")
+                     .Replace("\r", "");
+        string expectedJsonnowhite = expectedJson.Replace(" ", "")
+                     .Replace("\t", "")
+                     .Replace("\n", "")
+                     .Replace("\r", "");
+        Console.WriteLine(responseBodyone);
+        Console.WriteLine(expectedJson);
+        Assert.AreEqual(responseBodyonenowhite, expectedJsonnowhite);
+
      }
 }
